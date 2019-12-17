1a0e5d30315f0514d34222aaf67ee456e7f54284
Manuel Wecker <56833780+manuelwecker@users.noreply.github.com>
2019-12-17 14:08:18 +0100

    Add theming and global styles (#9)
    Theming and global styles integrated. 
    Fixed #6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { ThemeProvider } from "emotion-theming";
+import styled from "@emotion/styled";
+import light from "./themes/light";
+import dark from "./themes/dark";
+import GlobalStyles from "./GlobalStyles";
+
+const Headline = styled.h1`
+  font-weight: bold;
+  color: ${dark.colors.font};
+`;
 
 function App() {
   return (
-    <div>
-      <h1>Hello issue Tracker</h1>
-    </div>
+    <>
+      <ThemeProvider theme={light}>
+        <GlobalStyles />
+        <div>
+          <Headline>Hello issue Tracker</Headline>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 

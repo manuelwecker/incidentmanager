e2d36f0af99371b944b31148a309c644ddac3217
manuelwecker <manuelwecker@web.de>
2019-12-19 17:30:09 +0100

    Integrate theme switch for storybook via knobs

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -3,15 +3,31 @@ import { configure, addDecorator } from "@storybook/react";
 import GlobalStyles from "../src/GlobalStyles";
 import { ThemeProvider } from "emotion-theming";
 import light from "../src/themes/light";
+import dark from "../src/themes/dark";
 import { withKnobs } from "@storybook/addon-knobs";
+import { select } from "@storybook/addon-knobs";
+
+function chooseTheme(choice) {
+  if (choice === "Dark") {
+    return dark;
+  }
+  if (choice === "Light") {
+    return light;
+  }
+}
 
 // add GlobalStyle for every story
-const GlobalStyleDecorator = storyFn => (
-  <ThemeProvider theme={light}>
-    <GlobalStyles />
-    {storyFn()}
-  </ThemeProvider>
-);
+const GlobalStyleDecorator = storyFn => {
+  return (
+    <ThemeProvider
+      theme={chooseTheme(select("Choose Theme", ["Dark", "Light"], "Light"))}
+    >
+      <GlobalStyles />
+      {storyFn()}
+    </ThemeProvider>
+  );
+};
+
 addDecorator(GlobalStyleDecorator);
 addDecorator(withKnobs);
 

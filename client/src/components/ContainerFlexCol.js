060703e9c83bdf421150fa63e9ba3912b42a656e
manuelwecker <manuelwecker@web.de>
2020-01-09 13:41:17 +0100

    create container components for flex row and column

---
+++ client/src/components/ContainerFlexCol.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const ContainerFlexCol = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  vertical-align: middle;
+`;
+
+export default ContainerFlexCol;
---
+++ client/src/components/ContainerFlexRow.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const ContainerFlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  vertical-align: middle;
+`;
+
+export default ContainerFlexRow;

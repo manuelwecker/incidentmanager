478c9a55f9f56aa9e122784ce16e013a51093865
manuelwecker <manuelwecker@web.de>
2020-01-09 12:20:43 +0100

    change routes (exclude footer & header home)

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import Logo from "./components/Logo";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-import { ButtonText } from "./components/Buttons";
 import {
   ReportOne,
   ReportTwo,
@@ -18,6 +17,7 @@ import {
   SendMemo,
   StartScreen
 } from "./pages";
+
 const Headline = styled.h1`
   font-weight: bold;
   color: ${props => props.theme.colors.font};
@@ -31,52 +31,54 @@ const Container = styled.div`
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={light}>
-        <GlobalStyles />
-        <Router>
-          <Container>
-            <Header>
-              <Logo />
-            </Header>
-            <Main>
-              <Switch>
-                <Route path="/issues" component={IssueList}></Route>
-                <Route path="/report" component={ReportOne}></Route>
-                <Route path="/report-2" component={ReportTwo}></Route>
-                <Route path="/report-3" component={ReportThree}></Route>
-                <Route path="/report-4" component={ReportFour}></Route>
-                <Route path="/summary">
-                  <Headline>
-                    Report – Summary and last chance to edit the message before
-                    send
-                  </Headline>
-                </Route>
-                <Route path="/send" component={SendMemo}></Route>
+    <ThemeProvider theme={light}>
+      <GlobalStyles />
+      <Router>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={StartScreen} />
+            <Route>
+              <Header>
+                <Logo />
+              </Header>
+              <Main>
+                <Switch>
+                  <Route path="/issues" component={IssueList}></Route>
+                  <Route path="/report" component={ReportOne}></Route>
+                  <Route path="/report-2" component={ReportTwo}></Route>
+                  <Route path="/report-3" component={ReportThree}></Route>
+                  <Route path="/report-4" component={ReportFour}></Route>
+                  <Route path="/summary">
+                    <Headline>
+                      Report – Summary and last chance to edit the message
+                      before send
+                    </Headline>
+                  </Route>
+                  <Route path="/send" component={SendMemo}></Route>
 
-                <Route path="/tasks">
-                  <Headline>
-                    Tasks – overview of tasks after reporting the issue
-                  </Headline>
-                </Route>
-                <Route path="/login">
-                  <Headline>
-                    Login – Enter your user and password or register
-                  </Headline>
-                </Route>
-                <Route path="/settings">
-                  <Headline>
-                    Settings – Set up your company and personal profile
-                  </Headline>
-                </Route>
-                <Route path="/" component={StartScreen}></Route>
-              </Switch>
-            </Main>
-            <Footer></Footer>
-          </Container>
-        </Router>
-      </ThemeProvider>
-    </>
+                  <Route path="/tasks">
+                    <Headline>
+                      Tasks – overview of tasks after reporting the issue
+                    </Headline>
+                  </Route>
+                  <Route path="/login">
+                    <Headline>
+                      Login – Enter your user and password or register
+                    </Headline>
+                  </Route>
+                  <Route path="/settings">
+                    <Headline>
+                      Settings – Set up your company and personal profile
+                    </Headline>
+                  </Route>
+                </Switch>
+              </Main>
+              <Footer />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
 }
 

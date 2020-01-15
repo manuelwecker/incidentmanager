ed839d67dc93bcc1f13112ce0982c80fec80d0a7
manuelwecker <manuelwecker@web.de>
2020-01-15 12:09:27 +0100

    fix theme switch with new dark color

--- client/src/themes/dark.js
+++ client/src/themes/dark.js
@@ -9,8 +9,8 @@ const dark = {
     corporateDesignSecondary: "#00a6eb",
     background: "#333333",
 
-    primary: "#333333",
-    secondary: "#52565a",
+    primary: "#52565A",
+    secondary: "#333333",
     tertiary: "#7e8890",
 
     warningLevel: { 3: "#b70000", 2: "#f49d00", 1: "#7EB61C" },

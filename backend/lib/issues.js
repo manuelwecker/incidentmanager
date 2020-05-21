1cd8889b788d22064748712bed845a73af1831f8
leonp5 <leonpe@web.de>
2020-05-21 18:15:42 +0200

    Add lib functionality for fetching last issue

--- backend/lib/issues.js
+++ backend/lib/issues.js
@@ -9,11 +9,21 @@ function getIssues() {
   return collection.find().toArray();
 }
 
+function getLastIssue() {
+  const collection = getIssueCollection();
+  return collection
+    .find()
+    .sort({ _id: -1 })
+    .limit(1)
+    .toArray();
+}
+
 function addIssue(issue) {
   return getIssueCollection().insertOne(issue);
 }
 
 module.exports = {
   addIssue,
-  getIssues
+  getIssues,
+  getLastIssue
 };
--- backend/routes/dbRoutes.js
+++ backend/routes/dbRoutes.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const dbRoutes = express.Router();
 
-const { addIssue, getIssues } = require("../lib/issues");
+const { addIssue, getIssues, getLastIssue } = require("../lib/issues");
 
 dbRoutes.post("/issues", async (request, response) => {
   try {
     const attendee = request.body;
     await addIssue(attendee);
     response.end();
+  } catch (error) {
+    console.error(error);
+    response.status(500).end();
+  }
+});
+
+dbRoutes.get("/lastissue", async (request, response) => {
+  try {
+    const issue = await getLastIssue();
+    console.log(issue);
+    response.json(issue);
   } catch (error) {
     console.error(error);
     response.status(500).end();
